1b607ced05ba00fe1f2b75e04c6653d7136200e1
Alexandr Agafonov <agafonovfm@gmail.com>
2021-06-26 20:57:05 +0300

    v.0.1

--- ad_helper_bot.py
+++ ad_helper_bot.py
@@ -2,8 +2,11 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from states import Actions
 from aiogram.dispatcher import FSMContext
+from states import Actions
+import requests
+from bs4 import BeautifulSoup
+import json
 
 
 token = '1615105002:AAGPdbHvBXLiHCG4hxlB87YQIMt4DFCTQOA'
@@ -13,12 +16,11 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 # создаем клавиатуру
 show_yandex_add_button = InlineKeyboardButton('Конкуренты в Яндексе', callback_data='c_show_yandex_add')
-show_google_add_button = InlineKeyboardButton('Конкуренты в Google', callback_data='c_show_google_add')
-screen_yandex_add_button = InlineKeyboardButton('Скрин рекламы в Яндексе', callback_data='c_screen_yandex_add')
-screen_google_add_button = InlineKeyboardButton('Скрин рекламы в Google', callback_data='c_screen_google_add')
+screen_yandex_add_button = InlineKeyboardButton('Скрин выдачи в Яндексе', callback_data='c_screen_yandex_add')
+screen_google_add_button = InlineKeyboardButton('Скрин выдачи в Google', callback_data='c_screen_google_add')
 
 keyboard = InlineKeyboardMarkup(resize_keyboard=True)
-keyboard.add(show_yandex_add_button, show_google_add_button)
+keyboard.add(show_yandex_add_button)
 keyboard.add(screen_yandex_add_button, screen_google_add_button)
 
 
@@ -40,33 +42,32 @@ async def get_yandex_add_query(callback_query: types.CallbackQuery):
 @dp.message_handler(state=Actions.yandex_add_state)
 async def get_yandex_add_text(message: types.message, state: FSMContext):
     if message.text != '/start':
-        url = 'https://yandex.ru/search/?lr=1&text=' + message.text.replace(' ', '+')
-        await message.answer('Вот ваша ссылка на Яндекс, босс: ' + url, reply_markup=keyboard)
-        await state.finish()
+        url = 'https://www.yandex.ru/search/ads?text='
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+        search_query = message.text.replace(' ', '+')
+
+        req = requests.get(url + search_query + '&lr=1', headers=headers, stream=True)
+        soup = BeautifulSoup(req.content.decode('utf-8'), 'html.parser')
+
+        try:
+            for ad in soup.find_all('li', attrs={'class': 'serp-item'})[:3]:
+                head = ad.find('div', attrs={'class': 'OrganicTitle-LinkText'}).text  # заголовок
+                ad_text = ad.find('div', attrs={'class': 'Typo_text_m'}).text  # тексты
+                domain = ad.find('div', attrs={'class': 'Organic-Path'}).find('b').text  # видимый домен
+
+                await message.answer(head + '\n\n' + ad_text + '\n\n' + domain)
+
+        except:
+            await message.answer('По данному запросу нет рекламных объявлений')
+
+        finally:
+            await state.finish()
+            await message.answer('Чего изволите?', reply_markup=keyboard)
+
     else:
         await state.finish()
-        await message.answer('Привет! Чего изволите?', reply_markup=keyboard)
+        await message.answer('Чего изволите?', reply_markup=keyboard)
 # Блок запросов Яндекса
-
-
-# Блок запросов Google
-@dp.callback_query_handler(lambda c: c.data == 'c_show_google_add')  # обрабатываем клик по кнопке и ставим состояние
-async def get_google_add_query(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Введи поисковой запрос для Google')
-    await Actions.google_add_state.set()
-
-
-@dp.message_handler(state=Actions.google_add_state)
-async def get_google_add_text(message: types.message, state: FSMContext):
-    if message.text != '/start':
-        url = 'https://www.google.ru/search?q=' + message.text.replace(' ', '+')
-        await message.answer('Вот ваша ссылка на Google, босс: ' + url, reply_markup=keyboard)
-        await state.finish()
-    else:
-        await state.finish()
-        await message.answer('Привет! Чего изволите?', reply_markup=keyboard)
-# Блок запросов Google
 
 
 # блок скринов Яндекса
@@ -80,12 +81,38 @@ async def get_yandex_add_screenshot(callback_query: types.CallbackQuery):
 @dp.message_handler(state=Actions.yandex_screen_state)
 async def get_yandex_screen(message: types.message, state: FSMContext):
     if message.text != '/start':
-        await message.answer('Давай притворимся что это скриншот Яндекса по запросу "' + message.text + '" ?',
-                             reply_markup=keyboard)
+        search_query = message.text.replace(' ', '+')
+        yandex_ad_url = 'https://www.yandex.ru/search/ads?text=' + search_query + '&lr=1'
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+        url = ' https://api.topvisor.com/v2/json/get/webScreens_2'
+        api_key = '43012ad5e875832a46fe'
+
+        headers = {
+            'User-Id': '291326',
+            'Authorization': 'bearer ' + api_key,
+            'Content-type': 'application/json'
+        }
+
+        params = {
+            'url': yandex_ad_url,
+            'w': 1000,
+            'h': 2350,
+        }
+
+        body = json.dumps(params)
+        req = requests.post(url, body, headers=headers)
+
+        with open('myfile.png', 'wb') as f:
+            f.write(req.content)
+
+        screen = open('myfile.png', 'rb')
+        await message.answer_document(screen)
         await state.finish()
+        await message.answer('Чего изволите?', reply_markup=keyboard)
     else:
         await state.finish()
-        await message.answer('Привет! Чего изволите?', reply_markup=keyboard)
+        await message.answer('Чего изволите?', reply_markup=keyboard)
 # блок скринов Яндекса
 
 
@@ -100,13 +127,46 @@ async def get_google_add_screenshot(callback_query: types.CallbackQuery):
 @dp.message_handler(state=Actions.google_screen_state)
 async def get_google_screen(message: types.message, state: FSMContext):
     if message.text != '/start':
-        await message.answer('Давай притворимся что это скриншот Google по запросу "' + message.text + '" ?',
-                             reply_markup=keyboard)
+        search_query = message.text.replace(' ', '+')
+        yandex_ad_url = 'https://www.google.ru/search?q=' + search_query
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+
+        url = ' https://api.topvisor.com/v2/json/get/webScreens_2'
+        api_key = '43012ad5e875832a46fe'
+
+        headers = {
+            'User-Id': '291326',
+            'Authorization': 'bearer ' + api_key,
+            'Content-type': 'application/json'
+        }
+
+        params = {
+            'url': yandex_ad_url,
+            'w': 1000,
+            'h': 2350,
+        }
+
+        body = json.dumps(params)
+        req = requests.post(url, body, headers=headers)
+
+        with open('myfile.png', 'wb') as f:
+            f.write(req.content)
+
+        screen = open('myfile.png', 'rb')
+        await message.answer_document(screen)
         await state.finish()
+        await message.answer('Чего изволите?', reply_markup=keyboard)
     else:
         await state.finish()
-        await message.answer('Привет! Чего изволите?', reply_markup=keyboard)
+        await message.answer('Чего изволите?', reply_markup=keyboard)
 # блок скринов Goolge
+
+
+# запрос IP адресс - скрытый функционал
+@dp.message_handler(commands=['ip'])
+async def get_my_ip(callback_query: types.CallbackQuery):
+    req = requests.get('https://api.myip.com/')
+    await bot.send_message(callback_query.from_user.id, req.json()['ip'])
 
 
 executor.start_polling(dp)
